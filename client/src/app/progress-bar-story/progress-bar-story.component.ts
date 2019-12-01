28b1ef0f5530387ca6fb752cf76b5e5408eeec92
Ritu Raut <rituraut05@gmail.com>
2019-12-02 01:58:15 +0530

    improved progress bar text displayed

--- client/src/app/progress-bar-story/progress-bar-story.component.ts
+++ client/src/app/progress-bar-story/progress-bar-story.component.ts
@@ -25,26 +25,21 @@ import { take } from 'rxjs/operators';
 @Component({
   selector: 'clr-progress-bar-story-demo',
   template: `
-    <div class="story-message" style="margin-bottom: 0.5rem" width="50%">
+    <div class="story-message" style="margin-bottom: 1rem" width="50%">
       <p *ngIf="storyProgressBar.value <= 25">
-        <clr-icon shape="cog" size="24"></clr-icon>Generating Model Defination<clr-icon shape="ellipsis-horizontal" size="24"></clr-icon>
+        <clr-icon shape="cog" size="24"></clr-icon>Uploading Form<clr-icon shape="ellipsis-horizontal" size="24"></clr-icon>
       </p>
       <p *ngIf="storyProgressBar.value > 25 && storyProgressBar.value <= 50">
-        <clr-icon shape="install" size="24"></clr-icon>Generating Model Test Data<clr-icon shape="ellipsis-horizontal" size="24"></clr-icon>
+        <clr-icon shape="install" size="24"></clr-icon>Extracting Insurance Data<clr-icon shape="ellipsis-horizontal" size="24"></clr-icon>
       </p>
       <p *ngIf="storyProgressBar.value > 50 && storyProgressBar.value <= 75">
-        <clr-icon shape="download-cloud" size="24"></clr-icon>Generating Paths<clr-icon shape="ellipsis-horizontal" size="24"></clr-icon>
+        <clr-icon shape="download-cloud" size="24"></clr-icon>Verifying Insurance Data<clr-icon shape="ellipsis-horizontal" size="24"></clr-icon>
       </p>
       <p *ngIf="storyProgressBar.value > 75 && storyProgressBar.value < 100">
-        <clr-icon shape="step-forward-2" size="24"></clr-icon>Generating Dependency Graph<clr-icon shape="ellipsis-horizontal" size="24"></clr-icon>
+        <clr-icon shape="step-forward-2" size="24"></clr-icon>Generating Structured Form Data<clr-icon shape="ellipsis-horizontal" size="24"></clr-icon>
       </p>
       <p *ngIf="storyProgressBar.value == 100">
         <clr-icon shape="success-standard" size="24"></clr-icon>Processing Done
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Added:
-        <span class="label label-purple" style="display: inline" >Model Defination</span>
-        <span class="label label-purple" style="display: inline" >Model Test Data</span>
-        <span class="label label-purple" style="display: inline" >Path Defination</span>
-        <span class="label label-purple" style="display: inline" >Dependency Graph</span>
       </p>
     </div>
     <div [ngClass]="storyProgressBar.cssClassnames()">
