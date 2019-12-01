18604e0dc7efd06e34987708b7c4b6a10e720a78
Ritu Raut <rituraut05@gmail.com>
2019-12-02 02:37:32 +0530

    Adder loader after check_risk and some mninor changes

--- client/src/app/in-dashboard/in-dashboard.component.ts
+++ client/src/app/in-dashboard/in-dashboard.component.ts
@@ -9,90 +9,97 @@ export class InDashboardComponent implements OnInit {
 
   constructor() { }
   check_risk = false;
-  toggle_check_risk(){
-    this.check_risk = true
+  loading = false;
+  toggle_check_risk() {
+    // this.check_risk = true;
+    this.loading = true;
+    setTimeout (() => {
+      this.loading = false;
+      this.check_risk = true;
+   }, 3000);
   }
+  // tslint:disable-next-line: member-ordering
   contracts = [{
-    farmer_name: "Shivram Bhau Patil",
-    type: "Flood Insurance",
-    region: "Jalgaon, Maharashtra",
-    issue: "28/11/2019",
-    maturity: "29/11/2024",
-    premium: "1000 Rs",
-    recurrence: "Monthly",
+    farmer_name: 'Shivram Bhau Patil',
+    type: 'Flood Insurance',
+    region: 'Jalgaon, Maharashtra',
+    issue: '28/11/2019',
+    maturity: '29/11/2024',
+    premium: '1000 Rs',
+    recurrence: 'Monthly',
     due: true,
     damage: 90
   },
   {
-    farmer_name: "Shivram Bhau Patil",
-    type: "Flood Insurance",
-    region: "Jalgaon, Maharashtra",
-    issue: "28/11/2019",
-    maturity: "29/11/2024",
-    premium: "1000 Rs",
-    recurrence: "Monthly",
+    farmer_name: 'Shivram Bhau Patil',
+    type: 'Flood Insurance',
+    region: 'Jalgaon, Maharashtra',
+    issue: '28/11/2019',
+    maturity: '29/11/2024',
+    premium: '1000 Rs',
+    recurrence: 'Monthly',
     due: false,
     damage: 20
   },
   {
-    farmer_name: "Shivram Bhau Patil",
-    type: "Flood Insurance",
-    region: "Jalgaon, Maharashtra",
-    issue: "28/11/2019",
-    maturity: "29/11/2024",
-    premium: "1000 Rs",
-    recurrence: "Monthly",
+    farmer_name: 'Shivram Bhau Patil',
+    type: 'Flood Insurance',
+    region: 'Jalgaon, Maharashtra',
+    issue: '28/11/2019',
+    maturity: '29/11/2024',
+    premium: '1000 Rs',
+    recurrence: 'Monthly',
     due: true,
     damage: 12
   },
   {
-    farmer_name: "Shivram Bhau Patil",
-    type: "Flood Insurance",
-    region: "Jalgaon, Maharashtra",
-    issue: "28/11/2019",
-    maturity: "29/11/2024",
-    premium: "1000 Rs",
-    recurrence: "Monthly",
+    farmer_name: 'Shivram Bhau Patil',
+    type: 'Flood Insurance',
+    region: 'Jalgaon, Maharashtra',
+    issue: '28/11/2019',
+    maturity: '29/11/2024',
+    premium: '1000 Rs',
+    recurrence: 'Monthly',
     due: false,
     damage: 82
   },
   {
-    farmer_name: "Shivram Bhau Patil",
-    type: "Flood Insurance",
-    region: "Jalgaon, Maharashtra",
-    issue: "28/11/2019",
-    maturity: "29/11/2024",
-    premium: "1000 Rs",
-    recurrence: "Monthly",
+    farmer_name: 'Shivram Bhau Patil',
+    type: 'Flood Insurance',
+    region: 'Jalgaon, Maharashtra',
+    issue: '28/11/2019',
+    maturity: '29/11/2024',
+    premium: '1000 Rs',
+    recurrence: 'Monthly',
     due: true,
     damage: 85
   },
   {
-    farmer_name: "Shivram Bhau Patil",
-    type: "Flood Insurance",
-    region: "Jalgaon, Maharashtra",
-    issue: "28/11/2019",
-    maturity: "29/11/2024",
-    premium: "1000 Rs",
-    recurrence: "Monthly",
+    farmer_name: 'Shivram Bhau Patil',
+    type: 'Flood Insurance',
+    region: 'Jalgaon, Maharashtra',
+    issue: '28/11/2019',
+    maturity: '29/11/2024',
+    premium: '1000 Rs',
+    recurrence: 'Monthly',
     due: true,
     damage: 23
   },
   {
-    farmer_name: "Shivram Bhau Patil",
-    type: "Flood Insurance",
-    region: "Jalgaon, Maharashtra",
-    issue: "28/11/2019",
-    maturity: "29/11/2024",
-    premium: "1000 Rs",
-    recurrence: "Monthly",
+    farmer_name: 'Shivram Bhau Patil',
+    type: 'Flood Insurance',
+    region: 'Jalgaon, Maharashtra',
+    issue: '28/11/2019',
+    maturity: '29/11/2024',
+    premium: '1000 Rs',
+    recurrence: 'Monthly',
     due: false,
     damage: 50
   }
 
 ];
   items = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-  option = "lazy";
+  option = 'lazy';
   downloadinInvoice = true;
   ngOnInit() {
   }
