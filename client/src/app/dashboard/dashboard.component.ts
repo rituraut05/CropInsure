ea48906a8dddd394aa287fcecddc467c02e92189
Ritu Raut <rituraut05@gmail.com>
2019-12-02 04:58:08 +0530

    Added model and changed dashboard structure

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -15,6 +15,49 @@ export class DashboardComponent implements OnInit {
     history.pushState({data: {key: 'valuee'}}, '', '');
     this.router.navigate(['/InsuranceForm'], {state: {data: {key: 'valuee'}}});
   }
+  basic = false
+  modal(){
+    this.basic = true
+  }
+
+  contracts = [{
+    farmer_name: 'Shivram Bhau Patil',
+    type: 'Flood Insurance',
+    region: 'Jalgaon, Maharashtra',
+    issue: '28/11/2019',
+    maturity: '29/11/2024',
+    premium: '1000 Rs',
+    recurrence: 'Monthly',
+    due: true,
+    claim: false,
+    damage: 90
+  },
+  {
+    farmer_name: 'Shivram Bhau Patil',
+    type: 'Wasp Disease Insurance',
+    region: 'Jalgaon, Maharashtra',
+    issue: '11/5/2017',
+    maturity: '11/4/2011',
+    premium: '500 Rs',
+    recurrence: 'Monthly',
+    due: false,
+    claim: true,
+    damage: 20
+  },
+  {
+    farmer_name: 'Shivram Bhau Patil',
+    type: 'Drought Insurance',
+    region: 'Jalgaon, Maharashtra',
+    issue: '10/7/2018',
+    maturity: '11/11/2010',
+    premium: '800 Rs',
+    recurrence: 'Monthly',
+    due: true,
+    claim: false,
+    damage: 12
+  }
+  
+];
 
   ngOnInit() {
   }
